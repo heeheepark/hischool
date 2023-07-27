4e01ce073a80937b8a513d6cf989f38ffe1f3ca8
gyeongisung <gisung.gyeon@gmail.com>
2023-07-27 18:04:38 +0900

    feat: login,signup 작업중

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,32 +1,49 @@
 import React from "react";
-import { IntroImage } from "../../styles/LoginStyle";
+import {
+  IntroImage,
+  LoginContain,
+  LoginForm,
+  LoginWrap,
+} from "../../styles/LoginStyle";
 import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
+    <LoginWrap>
       <div>
         <IntroImage src="../images/intro.png" />
       </div>
-      <div>
-        <form>
-          <span>Login</span>
-          <input type="radio" id="관리자" name="난 교사고 넌 학생이야?" />
-          <label htmlFor="관리자">관리자</label>
-          <input type="radio" id="선생님" name="난 교사고 넌 학생이야?" />
-          <label htmlFor="선생님">선생님</label>
-          <input type="radio" id="학생" name="난 교사고 넌 학생이야?" />
-          <label htmlFor="학생">학생</label>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="PassWord" />
-          <button type="submit">Login</button>
-        </form>
-      </div>
-      <div>
-        <Link to="#">Email/PW 찾기</Link>
-        <Link to="/signup">회원가입</Link>
-      </div>
-    </div>
+      <LoginContain>
+        <span>Login</span>
+        <LoginForm>
+          <ul>
+            <li>
+              <input type="radio" id="관리자" name="난 교사고 넌 학생이야?" />
+              <label htmlFor="관리자">관리자</label>
+            </li>
+            <li>
+              <input type="radio" id="선생님" name="난 교사고 넌 학생이야?" />
+              <label htmlFor="선생님">선생님</label>
+            </li>
+            <li>
+              <input type="radio" id="학생" name="난 교사고 넌 학생이야?" />
+              <label htmlFor="학생">학생</label>
+            </li>
+          </ul>
+          <div>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="PassWord" />
+          </div>
+          <div className="login-button">
+            <button type="submit">Login</button>
+          </div>
+          <div className="link-button">
+            <Link to="#">Email/PW 찾기</Link>
+            <Link to="/signup">회원가입</Link>
+          </div>
+        </LoginForm>
+      </LoginContain>
+    </LoginWrap>
   );
 };
 
