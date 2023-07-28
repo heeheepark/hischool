ee23730c1d2c0509df1990e93571ac3693c26f29
gyeongisung <gisung.gyeon@gmail.com>
2023-07-28 16:29:07 +0900

    feat: Login,SignUp 퍼블리싱 및 기능 구현 작업 중

--- src/styles/LoginStyle.js
+++ src/styles/LoginStyle.js
@@ -5,24 +5,25 @@ export const LoginWrap = styled.div`
   height: 100vh;
 `;
 
-export const IntroImage = styled.img`
+export const IntroImage = styled.div`
   width: 100vw;
   height: 50vh;
+  img {
+    width: 100%;
+    height: 100%;
+  }
 `;
 
 export const LoginContain = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-`;
-
-export const LoginForm = styled.form`
   font-size: 20px;
-  margin-right: 20%;
+  margin-top: 8%;
+  margin-right: 35px;
   ul {
     font-size: 35px;
     display: flex;
-    gap: 15px;
+    justify-content: space-around;
+    align-items: center;
+    gap: 0 15px;
     li {
       font-size: 20px;
       input[type="radio"] {
@@ -31,23 +32,46 @@ export const LoginForm = styled.form`
         margin-right: 5px;
         vertical-align: -3.5px;
       }
+      > span {
+        font-family: "yg-jalnan";
+        font-size: 40px;
+      }
     }
   }
-  > div {
-    > input {
+  div {
+    width: 400px;
+    input {
       display: block;
       padding-left: 20px;
       padding-right: 20px;
       width: 100%;
-      height: 45px;
+      height: 60px;
       border-radius: 20px;
       margin: 20px 0;
     }
   }
-  > span {
+  .login-email {
+    font-family: "HakgyoansimWoojuR";
+    font-weight: 600;
+    font-size: 20px;
+  }
+  .login-password {
+    font-family: "HakgyoansimWoojuR";
+    font-weight: 600;
+    font-size: 20px;
+  }
+`;
+
+export const LoginForm = styled.form`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  span {
     font-size: 45px;
   }
   .login-button {
+    margin-top: 9.5%;
+    padding-right: 10%;
     button {
       width: 100px;
       height: 100px;
@@ -56,8 +80,11 @@ export const LoginForm = styled.form`
       background: #176b87;
       border: none;
       color: #fff;
+      cursor: pointer;
     }
   }
   .link-button {
+    display: flex;
+    justify-content: space-around;
   }
 `;
--- src/styles/login/SignUpStyle.js
+++ src/styles/login/SignUpStyle.js
@@ -1,58 +1,114 @@
 import styled from "@emotion/styled";
 
 export const SignUpWrap = styled.div`
-  font-size: 30px;
+  font-size: 27px;
 `;
 
 export const SignUpInner = styled.div`
-  > div {
-    > form {
-      > input[type="radio"] {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  .signup-box {
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    width: auto;
+  }
+  .signup-radio {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 10px;
+    span {
+      font-family: "yg-jalnan";
+      font-size: 40px;
+      margin-right: 5%;
+    }
+    form {
+      input[type="radio"] {
         width: 20px;
         height: 20px;
-        margin-right: 5px;
+        margin-right: 8px;
+        vertical-align: -2px;
       }
+      label {
+        font-size: 27px;
+        margin-right: 10px;
+      }
+    }
+  }
+  .signup-submit {
+    display: flex;
+    justify-content: flex-end;
+    gap: 10px;
+    margin-top: 20px;
+    button {
+      width: 80px;
+      height: 30px;
+      font-family: "HakgyoansimWoojuR";
+      /* font-family: "yg-jalnan"; */
+      font-weight: 600;
+      font-size: 20px;
+      cursor: pointer;
+      border: none;
+      border-radius: 5px;
+      background: #176b87;
+      color: #fff;
     }
   }
 `;
 
 export const SignUpContain = styled.div`
+  display: flex;
+  justify-content: center;
+  width: auto;
+  height: auto;
+  .input-form {
+    display: flex;
+    gap: 20px;
+  }
   .image-upload {
     width: 200px;
     height: 200px;
-    background: cyan;
-  }
-  button {
-    width: 80px;
-    height: 30px;
-    cursor: pointer;
-    /* border: none; */
-    background: #fff;
+    margin-top: 13px;
+    background: #176b87;
   }
 `;
 
 export const SignUpUl = styled.ul`
+  display: flex;
   .big-input {
-    > input {
+    input {
       display: block;
       width: 300px;
       height: 40px;
       padding-left: 20px;
       padding-right: 20px;
       border-radius: 20px;
+      font-size: 20px;
     }
   }
   .small-input {
-    > input {
-      display: block;
-      width: 135px;
-      height: 40px;
-      padding-left: 15px;
-      padding-right: 15px;
-      border-radius: 20px;
+    display: flex;
+    justify-content: space-between;
+    div {
+      display: flex;
+      flex-direction: column;
+      input {
+        display: block;
+        font-size: 20px;
+        width: 135px;
+        height: 40px;
+        padding-left: 15px;
+        padding-right: 15px;
+        border-radius: 20px;
+      }
     }
   }
 `;
 
 export const LeftForm = styled.li``;
-export const RightForm = styled.li``;
+export const RightForm = styled.li`
+  margin-left: 30px;
+`;
