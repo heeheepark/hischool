20fe23e75e229e7dd79a15363a2afef7d751ec25
heeheepark <pjh9641@gmail.com>
2023-08-06 00:21:45 +0900

    design: mypage UI 수정

--- src/components/teacher/TeacherMyPage.js
+++ src/components/teacher/TeacherMyPage.js
@@ -5,6 +5,8 @@ import {
   TcMyPageDownInfo,
   TcMyPageLeftInfo,
   TcMyPageRightInfo,
+  TcMyPageUserInfo,
+  TcMyPageSchoolInfo,
   TcMyPageTopInfo,
   TcMyPageWrap,
 } from "../../styles/teacher/TeacherMyPageStyle";
@@ -73,142 +75,286 @@ const TeacherMyPage = () => {
 
   return (
     <TcMyPageWrap onSubmit={handleSubmit}>
-      <div>
-        <div className="TcMypagePic">
-          {teacher.pic ? (
-            <img src={teacher.pic} alt="pic" />
-          ) : (
-            <img src={teacher.profilePic} alt="pic" />
-          )}
-
-          <input
-            type="file"
-            accept="image/jpg, image/png, image/jpeg"
-            onChange={handlePictureUpload}
-          />
+      <div className="mypage-top">
+        <div className="user-info">
+          <div className="user-info-wrap">
+            <div className="user-picture-wrap">
+              <div className="picture-img">
+                {teacher.pic ? (
+                  <img src={teacher.pic} alt="pic" />
+                ) : (
+                  <img src={teacher.profilePic} alt="pic" />
+                )}
+              </div>
+              <input
+                type="file"
+                accept="image/jpg, image/png, image/jpeg"
+                onChange={handlePictureUpload}
+              />
+            </div>
+            <div className="school-info">
+              <TcMyPageUserInfo>
+                <li>
+                  <label>이메일</label>
+                  <input
+                    type="email"
+                    name="email"
+                    value={teacher.email}
+                    readOnly
+                  />
+                </li>
+                <li>
+                  <label>비밀번호</label>
+                  <input
+                    type="password"
+                    name="password"
+                    value={teacher.password}
+                    onChange={handleChange}
+                  />
+                </li>
+                <li>
+                  <label>비밀번호 확인</label>
+                  <input
+                    type="password"
+                    name="confirmPassword"
+                    value={teacher.confirmPassword}
+                    onChange={handleChange}
+                  />
+                </li>
+                <li>
+                  <label>이름</label>
+                  <input type="text" name="tnm" value={teacher.tnm} readOnly />
+                </li>
+                <li>
+                  <label>생년월일</label>
+                  <input
+                    type="text"
+                    name="birth"
+                    value={`${teacher.birth.split("-")[0]}년 ${
+                      teacher.birth.split("-")[1]
+                    }월 ${teacher.birth.split("-")[2]}일`}
+                    readOnly
+                  />
+                </li>
+                <li>
+                  <label>연락처</label>
+                  <input
+                    type="text"
+                    name="phone"
+                    value={teacher.phone}
+                    onChange={handleChange}
+                  />
+                </li>
+                <li>
+                  <label>주소</label>
+                  <div className="address-wrap">
+                    <div>
+                      <input
+                        type="text"
+                        name="address"
+                        value={teacher.address}
+                        onChange={handleChange}
+                        readOnly
+                      />
+                      <button>주소 검색</button>
+                    </div>
+                    <input
+                      type="text"
+                      name="detailAddress"
+                      className="detail-address"
+                      value={teacher.detailAddress}
+                      onChange={handleChange}
+                      placeholder="상세 주소를 입력하세요."
+                    />
+                  </div>
+                </li>
+                <li>
+                  <label>소속 학교</label>
+                  <div>
+                    <input
+                      type="text"
+                      name="snm"
+                      value={teacher.snm}
+                      onChange={handleChange}
+                      readOnly
+                    />
+                    <div>
+                      <input
+                        type="text"
+                        name="grade"
+                        value={`${teacher.grade}학년`}
+                        onChange={handleChange}
+                        readOnly
+                      />
+                      <input
+                        type="text"
+                        name="van"
+                        value={`${teacher.van}반`}
+                        onChange={handleChange}
+                        readOnly
+                      />
+                    </div>
+                  </div>
+                </li>
+                <li></li>
+              </TcMyPageUserInfo>
+              <TcMyPageRightInfo>
+                <ul></ul>
+              </TcMyPageRightInfo>
+            </div>
+          </div>
         </div>
-        <TcMyPageTopInfo>
-          <li>
-            <label>이름</label>
-            <input
-              type="text"
-              name="tnm"
-              value={teacher.tnm}
-              onChange={handleChange}
-            />
-          </li>
-          <li>
-            <label>생년월일</label>
-            <input
-              type="text"
-              name="birth"
-              value={teacher.birth}
-              onChange={handleChange}
-            />
-          </li>
-          <li>
-            <label>연락처</label>
-            <input
-              type="text"
-              name="phone"
-              value={teacher.phone}
-              onChange={handleChange}
-            />
-          </li>
-          <li>
-            <label>주소</label>
-            <input
-              type="text"
-              name="address"
-              value={teacher.address}
-              onChange={handleChange}
-            />
-          </li>
-          <li>
-            <label>상세주소</label>
-            <input
-              type="text"
-              name="detailAddress"
-              value={teacher.detailAddress}
-              onChange={handleChange}
-            />
-          </li>
-        </TcMyPageTopInfo>
       </div>
-      <TcMyPageDownInfo>
-        <TcMyPageLeftInfo>
-          <ul>
-            <li>
-              <label>학교</label>
-              <input
-                type="text"
-                name="snm"
-                value={teacher.snm}
-                onChange={handleChange}
-              />
-            </li>
-            <li className="grade-van">
-              <div>
-                <label>학년</label>
-                <input
-                  type="text"
-                  name="grade"
-                  value={teacher.grade}
-                  onChange={handleChange}
-                />
-              </div>
-              <div>
-                <label>반</label>
-                <input
-                  type="text"
-                  name="van"
-                  value={teacher.van}
-                  onChange={handleChange}
-                />
-              </div>
-            </li>
-          </ul>
-        </TcMyPageLeftInfo>
-        <TcMyPageRightInfo>
-          <ul>
-            <li>
-              <label>이메일</label>
-              <input
-                type="email"
-                name="email"
-                value={teacher.email}
-                onChange={handleChange}
-              />
-            </li>
-            <li>
-              <label>비밀번호</label>
-              <input
-                type="password"
-                name="password"
-                value={teacher.password}
-                onChange={handleChange}
-              />
-            </li>
-            <li>
-              <label>비밀번호 확인</label>
-              <input
-                type="password"
-                name="confirmPassword"
-                value={teacher.confirmPassword}
-                onChange={handleChange}
-              />
-            </li>
-          </ul>
-        </TcMyPageRightInfo>
-      </TcMyPageDownInfo>
-      <TcButtons>
-        <button type="button">회원탈퇴</button>
-        <div>
-          <button type="submit">수정</button>
-          <button type="button">취소</button>
-        </div>
-      </TcButtons>
+      <div className="mypage-bottom">
+        <TcButtons>
+          <button type="button" className="withdraw-btn">
+            회원탈퇴
+          </button>
+          <div>
+            <button type="submit">수정</button>
+            <button type="button" className="cancel-btn">
+              취소
+            </button>
+          </div>
+        </TcButtons>
+      </div>
     </TcMyPageWrap>
+    // <TcMyPageWrap onSubmit={handleSubmit}>
+    //   <div>
+    //     <div className="TcMypagePic">
+    //       {teacher.pic ? (
+    //         <img src={teacher.pic} alt="pic" />
+    //       ) : (
+    //         <img src={teacher.profilePic} alt="pic" />
+    //       )}
+    //       <input
+    //         type="file"
+    //         accept="image/jpg, image/png, image/jpeg"
+    //         onChange={handlePictureUpload}
+    //       />
+    //     </div>
+    //     <TcMyPageTopInfo>
+    //       <li>
+    //         <label>이름</label>
+    //         <input
+    //           type="text"
+    //           name="tnm"
+    //           value={teacher.tnm}
+    //           onChange={handleChange}
+    //         />
+    //       </li>
+    //       <li>
+    //         <label>생년월일</label>
+    //         <input
+    //           type="text"
+    //           name="birth"
+    //           value={teacher.birth}
+    //           onChange={handleChange}
+    //         />
+    //       </li>
+    //       <li>
+    //         <label>연락처</label>
+    //         <input
+    //           type="text"
+    //           name="phone"
+    //           value={teacher.phone}
+    //           onChange={handleChange}
+    //         />
+    //       </li>
+    //       <li>
+    //         <label>주소</label>
+    //         <input
+    //           type="text"
+    //           name="address"
+    //           value={teacher.address}
+    //           onChange={handleChange}
+    //         />
+    //       </li>
+    //       <li>
+    //         <label>상세주소</label>
+    //         <input
+    //           type="text"
+    //           name="detailAddress"
+    //           value={teacher.detailAddress}
+    //           onChange={handleChange}
+    //         />
+    //       </li>
+    //     </TcMyPageTopInfo>
+    //   </div>
+    //   <TcMyPageDownInfo>
+    //     <TcMyPageLeftInfo>
+    //       <ul>
+    //         <li>
+    //           <label>학교</label>
+    //           <input
+    //             type="text"
+    //             name="snm"
+    //             value={teacher.snm}
+    //             onChange={handleChange}
+    //           />
+    //         </li>
+    //         <li className="grade-van">
+    //           <div>
+    //             <label>학년</label>
+    //             <input
+    //               type="text"
+    //               name="grade"
+    //               value={teacher.grade}
+    //               onChange={handleChange}
+    //             />
+    //           </div>
+    //           <div>
+    //             <label>반</label>
+    //             <input
+    //               type="text"
+    //               name="van"
+    //               value={teacher.van}
+    //               onChange={handleChange}
+    //             />
+    //           </div>
+    //         </li>
+    //       </ul>
+    //     </TcMyPageLeftInfo>
+    //     <TcMyPageRightInfo>
+    //       <ul>
+    //         <li>
+    //           <label>이메일</label>
+    //           <input
+    //             type="email"
+    //             name="email"
+    //             value={teacher.email}
+    //             onChange={handleChange}
+    //           />
+    //         </li>
+    //         <li>
+    //           <label>비밀번호</label>
+    //           <input
+    //             type="password"
+    //             name="password"
+    //             value={teacher.password}
+    //             onChange={handleChange}
+    //           />
+    //         </li>
+    //         <li>
+    //           <label>비밀번호 확인</label>
+    //           <input
+    //             type="password"
+    //             name="confirmPassword"
+    //             value={teacher.confirmPassword}
+    //             onChange={handleChange}
+    //           />
+    //         </li>
+    //       </ul>
+    //     </TcMyPageRightInfo>
+    //   </TcMyPageDownInfo>
+    //   <TcButtons>
+    //     <button type="button">회원탈퇴</button>
+    //     <div>
+    //       <button type="submit">수정</button>
+    //       <button type="button">취소</button>
+    //     </div>
+    //   </TcButtons>
+    // </TcMyPageWrap>
   );
 };
 
--- src/styles/teacher/TeacherMyPageStyle.js
+++ src/styles/teacher/TeacherMyPageStyle.js
@@ -1,88 +1,144 @@
 import styled from "@emotion/styled";
 
-export const TcMyPageWrap = styled.div`
-  padding: 5% 5% 0 5%;
-  list-style: none;
-  > div {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    gap: 40px;
-    .TcMypagePic {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      width: 200px;
-      height: 200px;
-      border: 1px solid yellow;
-      background: crimson;
-      input {
-      }
-    }
+export const MypageDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #f7f7f7;
+  padding: 60px 300px;
+  box-shadow: 0px 0.5px 5px 0px rgba(0, 0, 0, 0.2);
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: start;
+  h3 {
+    font-size: 30px;
+    margin-bottom: 30px;
   }
 `;
 
-export const TcMyPageTopInfo = styled.div`
+export const TcMyPageWrap = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
-  justify-content: center;
-  flex-direction: column;
-  vertical-align: middle;
-  gap: 10px;
-  font-size: 20px;
-  font-weight: 800;
-  li {
-    gap: 15px;
+  flex-flow: column nowrap;
+  justify-content: space-between;
+  .mypage-top {
     display: flex;
     justify-content: space-between;
-    input {
-      border-radius: 20px;
-      font-size: 18px;
-      height: 30px;
-      width: 300px;
-      padding: 0 10px;
-    }
-  }
-`;
-
-export const TcMyPageDownInfo = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding-top: 5%;
-`;
-
-export const TcMyPageLeftInfo = styled.div`
-  gap: 10px;
-  font-size: 20px;
-  font-weight: 800;
-  ul {
-    li {
-      input {
-        display: block;
-        border-radius: 20px;
-        font-size: 18px;
-        height: 30px;
-        width: 300px;
-        padding: 0 10px;
-        margin: 5px 0;
-      }
-    }
-    .grade-van {
-      display: flex;
-      justify-content: space-between;
-      div {
-        input {
-          font-size: 20px;
-          width: 100px;
-          height: 30px;
-          padding-left: 25%;
-          padding-right: 25%;
-          border-radius: 20px;
-          margin: 5px 0;
+    .user-info {
+      width: 100%;
+      height: 100%;
+      .user-info-wrap {
+        display: flex;
+        justify-content: space-between;
+        .user-picture-wrap {
+          display: flex;
+          flex-flow: column;
+          align-items: start;
+          margin-right: 20px;
+          .picture-img {
+            width: 300px;
+            height: 300px;
+            border: 1px solid gray;
+            background: gray;
+            margin-bottom: 10px;
+          }
         }
       }
     }
   }
 `;
+
+export const TcMyPageUserInfo = styled.ul`
+  width: 100%;
+  font-size: 21px;
+  font-weight: 500;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 20px;
+  li {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    label {
+      display: inline-block;
+      width: 150px;
+    }
+    input {
+      width: 21vw;
+      height: 35px;
+      border: 1px solid #bbb;
+      border-radius: 3px;
+      padding-left: 10px;
+      margin-right: 5px;
+      font-size: 18px;
+    }
+    :nth-of-type(7) {
+      align-items: start;
+      .address-wrap {
+        display: flex;
+        flex-flow: column nowrap;
+        align-items: start;
+        gap: 10px;
+        > div {
+          display: flex;
+          align-items: center;
+          button {
+            width: 80px;
+            height: 35px;
+            font-size: 15px;
+            color: #333;
+            border: 1px solid #bbb;
+            border-radius: 3px;
+            background: #ccc;
+            cursor: pointer;
+          }
+        }
+      }
+    }
+    :nth-of-type(8) {
+      display: flex;
+      align-items: start;
+      > div {
+        > div {
+          margin-top: 10px;
+          input {
+            width: 10.35vw;
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const TcMyPageSchoolInfo = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 20px;
+  font-size: 21px;
+  font-weight: 500;
+  li {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    label {
+      display: inline-block;
+      width: 100px;
+    }
+    input {
+      width: 12vw;
+      height: 35px;
+      border: 1px solid #bbb;
+      border-radius: 3px;
+      padding-left: 10px;
+      margin-right: 5px;
+      font-size: 18px;
+    }
+  }
+`;
+
 export const TcMyPageRightInfo = styled.div`
   margin-right: 30%;
   font-size: 20px;
@@ -102,23 +158,157 @@ export const TcMyPageRightInfo = styled.div`
 `;
 
 export const TcButtons = styled.div`
+  width: 100%;
   display: flex;
   justify-content: space-between;
-  padding-top: 12%;
-  padding-right: 10%;
   button {
     width: 80px;
-    height: 28px;
-    font-size: 16px;
-    font-weight: 600;
-    border: none;
-    border-radius: 5px;
-    background: #176b87;
+    height: 35px;
+    font-size: 15px;
     color: #fff;
+    border: none;
+    border-radius: 3px;
+    background: #64ccc5;
     cursor: pointer;
   }
   div {
-    display: flex;
-    gap: 15px;
+    button {
+      :first-of-type {
+        margin-right: 10px;
+        background: #176b87;
+        border: none;
+        color: #fff;
+      }
+      :last-of-type {
+        border: 1px solid #ccc;
+        color: #000;
+        background: #fff;
+      }
+    }
   }
 `;
+
+// 기존 코드
+
+// export const TcMyPageWrap = styled.div`
+//   padding: 5% 5% 0 5%;
+//   list-style: none;
+//   > div {
+//     width: 100%;
+//     display: flex;
+//     align-items: center;
+//     gap: 40px;
+//     .TcMypagePic {
+//       display: flex;
+//       align-items: center;
+//       justify-content: center;
+//       width: 200px;
+//       height: 200px;
+//       border: 1px solid yellow;
+//       background: crimson;
+//       input {
+//       }
+//     }
+//   }
+// `;
+
+// export const TcMyPageTopInfo = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   flex-direction: column;
+//   vertical-align: middle;
+//   gap: 10px;
+//   font-size: 20px;
+//   font-weight: 800;
+//   li {
+//     gap: 15px;
+//     display: flex;
+//     justify-content: space-between;
+//     input {
+//       border-radius: 20px;
+//       font-size: 18px;
+//       height: 30px;
+//       width: 300px;
+//       padding: 0 10px;
+//     }
+//   }
+// `;
+
+// export const TcMyPageDownInfo = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   padding-top: 5%;
+// `;
+
+// export const TcMyPageLeftInfo = styled.div`
+//   gap: 10px;
+//   font-size: 20px;
+//   font-weight: 800;
+//   ul {
+//     li {
+//       input {
+//         display: block;
+//         border-radius: 20px;
+//         font-size: 18px;
+//         height: 30px;
+//         width: 300px;
+//         padding: 0 10px;
+//         margin: 5px 0;
+//       }
+//     }
+//     .grade-van {
+//       display: flex;
+//       justify-content: space-between;
+//       div {
+//         input {
+//           font-size: 20px;
+//           width: 100px;
+//           height: 30px;
+//           padding-left: 25%;
+//           padding-right: 25%;
+//           border-radius: 20px;
+//           margin: 5px 0;
+//         }
+//       }
+//     }
+//   }
+// `;
+// export const TcMyPageRightInfo = styled.div`
+//   margin-right: 30%;
+//   font-size: 20px;
+//   font-weight: 800;
+//   li {
+//     padding-bottom: 10px;
+//     input {
+//       display: block;
+//       border-radius: 20px;
+//       font-size: 18px;
+//       height: 30px;
+//       width: 300px;
+//       padding: 0 10px;
+//       margin: 5px 0;
+//     }
+//   }
+// `;
+
+// export const TcButtons = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   padding-top: 12%;
+//   padding-right: 10%;
+//   button {
+//     width: 80px;
+//     height: 28px;
+//     font-size: 16px;
+//     font-weight: 600;
+//     border: none;
+//     border-radius: 5px;
+//     background: #176b87;
+//     color: #fff;
+//     cursor: pointer;
+//   }
+//   div {
+//     display: flex;
+//     gap: 15px;
+//   }
+// `;
