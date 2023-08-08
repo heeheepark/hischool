c55fd0155d0384c1892f2f572d2d454b5f9335c4
jooheepark <pjh9641@gmail.com>
2023-08-08 20:04:30 +0900

    feat: Login access token 갱신 오류 수정중

--- src/api/cookie.js
+++ src/api/cookie.js
@@ -2,10 +2,10 @@ import { Cookies } from "react-cookie";
 
 const cookies = new Cookies();
 
-export const setCookie = (_name, value) => {
-  return cookies.set(_name, value);
+export const setCookie = (name, value) => {
+  return cookies.set(name, value);
 };
 
-export const getCookie = _name => {
-  return cookies.get(_name);
+export const getCookie = name => {
+  return cookies.get(name);
 };
