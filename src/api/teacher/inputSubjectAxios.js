527721eb37abb24f57642fcdfb556c58565079c3
devicepac <bardichedevice@gmail.com>
2023-08-09 15:13:21 +0900

    Feat : 오류수정

--- src/api/teacher/inputSubjectAxios.js
+++ src/api/teacher/inputSubjectAxios.js
@@ -1,21 +1,21 @@
-import axios from "axios";
+import { client } from "../client";
 
 // 서버로 datatoMock 데이터를 전송하는 함수
 export const postALLData = async dataToSend => {
   try {
-    const response = await axios.post("/api/teacher/subject", {
+    const response = await client.post("/api/teacher/subject", {
       list: dataToSend,
     });
-    console.log("데이터 전송 성공:", response.data);
+    console.log("데이터 전송 성공 : ", response.data);
   } catch (error) {
     console.error("데이터 전송 오류:", error);
   }
 };
 
-// 모의 고사 과목 계열 가져오기
+// 내신 과목 계열 가져오기
 export const getALLMainSubData = async () => {
   try {
-    const res = await axios.get("/api/teacher/subject/category");
+    const res = await client.get("/api/teacher/subject/category");
     return res.data;
   } catch (err) {
     console.log(err);
@@ -23,10 +23,10 @@ export const getALLMainSubData = async () => {
   }
 };
 
-// 모의 고사 세부 과목 가져오기
+// 내신 세부 과목 가져오기
 export const getALLSubData = async categoryid => {
   try {
-    const res = await axios.get(
+    const res = await client.get(
       `/api/teacher/subject?categoryid=${categoryid}`,
     );
     return res.data;
