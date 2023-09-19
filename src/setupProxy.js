3b5e0c8963c0a09e757c189b12c94eecff6b0ed4
박주희(jooheepark) <pjh9641@gmail.com>
2023-09-19 13:09:15 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://192.168.0.144:5003/",
+      target: process.env.REACT_APP_API_URL,
       changeOrigin: true,
     }),
   );
